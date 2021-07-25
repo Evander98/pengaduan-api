f28f310b138a884a2edb7dc0ba1c8487dd743ef6
palitmario <mariopalit86@gmail.com>
2021-07-26 01:03:17 +0800

    test

--- database/index.js
+++ database/index.js
@@ -3,8 +3,9 @@ const mysql = require('mysql')
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '1234',
-  database: 'pengaduan'
+  password: '',
+  database: 'pengaduan',
+  port: '3306'
 })
 
 module.exports = db
